85164c1e79678e6a4a1d2cb18a99317d06dd8777
trivopia <135800895+trivopia@users.noreply.github.com>
2025-03-31 19:19:39 +0700

    modified:   sumAll.js modified:   sumAll.spec.js
    Solved problem number 05
    Check the solution for cleaner code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  let sum = 0;
 
+  if (x < y && x * y > 0 && Math.floor(x) === x && Math.floor(y) === y) {
+    for (let i = x; i <= y; i++) {
+      sum += i;
+    }
+  } else if (x > y) {
+    let a = x;
+    x = y;
+    y = a;
+
+    for (let i = x; i <= y; i++) {
+      sum += i;
+    }
+  } else {
+    return "ERROR";
+  }
+
+  return sum;
 };
 
+console.log(sumAll(1, 2));
 // Do not edit below this line
 module.exports = sumAll;
