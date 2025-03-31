a5080510e21eb378057011772a86b36d78b16014
trivopia <135800895+trivopia@users.noreply.github.com>
2025-03-31 16:41:14 +0700

    modified:   repeatString.js modified:   repeatString.spec.js
    Solved problem numebr 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (text, n) {
+  if (n < 0) {
+    return "ERROR";
+  } else if (text === "") {
+    return text;
+  }
 
+  let newString = "";
+
+  for (let i = 0; i < n; i++) {
+    newString += text;
+  }
+  return newString;
 };
 
 // Do not edit below this line
