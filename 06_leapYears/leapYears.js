42cc7cf1a855a5d2c7bbc4247cfcaafde9eacb0f
trivopia <135800895+trivopia@users.noreply.github.com>
2025-03-31 19:43:57 +0700

    modified:   leapYears.js modified:   leapYears.spec.js
    Solved problem number 06 with 2 types.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function (x) {
+  if (x % 4 === 0) {
+    if (x % 100 === 0) {
+      if (x % 400 === 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  } else {
+    return false;
+  }
 
+  // if (x % 4 === 0 && (!(x % 100 === 0) || x % 400 === 0)) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 };
 
 // Do not edit below this line
