db3af371ac32a11464c653e35d382be726d6ac93
trivopia <135800895+trivopia@users.noreply.github.com>
2025-03-31 18:49:13 +0700

    modified:   removeFromArray.js modified:   removeFromArray.spec.js
    Solved problem number 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...item) {
+  const newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (!item.includes(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 5], 1, 2, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
