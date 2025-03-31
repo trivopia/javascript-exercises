310d536b4a8864f2dc6e842adf760cc5ff58d463
trivopia <135800895+trivopia@users.noreply.github.com>
2025-03-31 18:07:50 +0700

    modified:   reverseString.js modified:   reverseString.spec.js
    Solved problem numer 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+  const reversed = text.split("").reverse().join("");
+  return reversed;
 };
 
+console.log(reverseString("hello world"));
 // Do not edit below this line
 module.exports = reverseString;
